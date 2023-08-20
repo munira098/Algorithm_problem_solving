dc957a55c65a18234d465697abccb6d817114e11
munira098 <99261347+munira098@users.noreply.github.com>
2023-08-20 15:14:00 +0600

    Create test.js

---
+++ second_smallest_number/test.js
@@ -0,0 +1,9 @@
+const array=[39,45,12,43,87,-23,872,974,6752];
+//const size=array.length;
+const result= sortarray(array);
+console.time();
+console.log("second smallest number is: ",result);
+//const size= arrlength;
+
+
+console.timeEnd();
