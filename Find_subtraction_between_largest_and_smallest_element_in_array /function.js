907b3ea7f9afeb4c4d38a82fb18dde8c6ffe646c
munira098 <99261347+munira098@users.noreply.github.com>
2023-08-20 15:09:55 +0600

    Update function.js

--- Find_subtraction_between_largest_and_smallest_element_in_array /function.js
+++ Find_subtraction_between_largest_and_smallest_element_in_array /function.js
@@ -1,1 +1,37 @@
 
+/* problem name: 11. Find the subtraction between largest and smallest element in array.
+ sample [8, 23, 43,11, 12, 54, 76, 98, 32]
+ result=90*/
+
+
+const getSmallestNumber = (list) => {
+    const arrlength= list.length;
+   let sub;
+    let min= list[0];
+
+    for(let i=0;i<arrlength ;i++)
+{
+
+    if (list[i]<=min){
+
+        min=list[i];
+    }
+
+}
+
+let max= list[0];
+
+for(let i=0;i<arrlength ;i++)
+{
+
+if (list[i]>=max){
+
+    max=list[i];
+}
+
+}
+sub= max-min;
+
+return sub;
+
+};
