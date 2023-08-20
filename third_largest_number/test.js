d5a01201b6c13284f7abc295de61bef99e09c47b
munira098 <99261347+munira098@users.noreply.github.com>
2023-08-20 15:31:53 +0600

    Create test.js

---
+++ third_largest_number/test.js
@@ -0,0 +1,8 @@
+const array=[27,38,903,86,-99,-76,32,98,865];
+//const size=array.length;
+const result= sortarray(array);
+console.time();
+console.log("third largest number is: ",result);
+
+
+console.timeEnd();
