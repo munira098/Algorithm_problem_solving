ab7289658fdea22257428e9dc2911cc2d02a92db
munira098 <99261347+munira098@users.noreply.github.com>
2023-08-23 20:19:32 +0600

    Create function.js

---
+++ find_area_and_perimeter_of_retriangle/function.js
@@ -0,0 +1,9 @@
+function areaRectangle(a, b) {
+    let area = a * b;
+    return area;
+}
+
+// Function to Find the Parameter of retriangle
+function perimeterRectangle(a, b) {
+    let perimeter = 2 * (a + b);
+    return perimeter;
